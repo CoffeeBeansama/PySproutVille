6c9e6b35299995b456d7c0e2a1d83e7fbd92250b
CoffeeBeansama <103745801+CoffeeBeansama@users.noreply.github.com>
2023-07-28 16:16:53 +0800

    update

---
+++ Map/tileset/Gras1s.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Grass" tilewidth="48" tileheight="48" tilecount="6" columns="3">
+ <image source="../../Sprites/Sprout Lands - Sprites - Basic pack/Tilesets/Grass.png" width="160" height="128"/>
+</tileset>
---
+++ Map/tileset/Grass111.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Grass111" tilewidth="32" tileheight="32" tilecount="20" columns="5">
+ <image source="../../Sprites/Sprout Lands - Sprites - Basic pack/Tilesets/Grass.png" width="160" height="128"/>
+</tileset>
